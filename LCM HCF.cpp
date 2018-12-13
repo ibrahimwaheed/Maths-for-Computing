0f731eab8d95c712037e86d896f2de9bb36d80e1
ibrahimwaheed <43203098+ibrahimwaheed@users.noreply.github.com>
2018-12-13 21:40:49 +0000

    Update LCM HCF.cpp

--- LCM HCF.cpp
+++ LCM HCF.cpp
@@ -2,25 +2,28 @@
 #include <string>
 
 using namespace std;
-
+//Beginning of the program
 int main() 
 	{
-		int dogs,buns,c;
+	//Declaring the variables that will be used as integers
+		int dogs,buns,LCM;
 		cout<< "Enter the amount of hot dogs and buns you have "<<endl;
 		cout<< "How many Hot Dogs do you have? ";
-		cin>>dogs;
+		cin>>dogs; //the amount of hotdogs is taken from the user
 		cout<< "How many Buns do you have? ";
-		cin>>buns;
-		c = dogs*buns;
-		while(dogs!=buns)
+		cin>>buns; //the amount of buns is taken from the user
+		LCM = dogs*buns; //The LCM is the product of the 2 numbers
+		while(dogs!=buns) // runs when the number of hotdogs is not equal to number of buns
 			{
-				if (dogs>buns)
-					dogs = dogs-buns;
+				if (dogs>buns)//if the amount of hotdogs are greater than amount of buns then-
+					dogs = dogs-buns;//the No. of buns is subtracted by the No. of hotdogs and the result is set as the new value for No. of hotdogs
 				else
-					buns = buns-dogs;
+					buns = buns-dogs;//the No. of hotdogs is subtracted by the No. of buns and the result is set as the new value for No. of buns
 			}
-		cout<< "The number of items per package are "<<a<<endl;
-		cout<< "The minimum amount of servings to be catered for "<< c/a <<endl;
+	//Display's the HCF
+		cout<< "The number of items per package are "<<dogs<<endl;//the new result for amount of hotdogs is set as the HCF
+	//Display's the LCM
+		cout<< "The minimum amount of servings to be catered for "<< LCM/dogs <<endl; //The minimum servings is the LCM
 
 		
 return 0;
